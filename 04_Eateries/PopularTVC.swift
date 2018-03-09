96bb4b2fdffb8dc3b2c8d7f27b133d0d05e93c6f
Admin <admin@mac-admin.local>
2018-03-09 23:26:48 +0200

    сделал подгрузку картинок с сайта

--- 04_Eateries/PopularTVC.swift
+++ 04_Eateries/PopularTVC.swift
@@ -12,17 +12,21 @@ class PopularTVC: UITableViewController {
     
     
     var someNames = ["Марадон", "лебякин", "Джун", "Шмякс", "Трякс", "Куня", "Зиба", "Вороба", "Бодитан", "Леминг", "Лушпак", "Кеды", "Козуб", "Дуря", "Ремминг Буболеховое Счастье нога-лицо"]
-    var somePics = ["ogonek.jpg", "elu.jpg", "bonsai.jpg", "dastarhan.jpg", "indokitay.jpg", "x.o.jpg", "balkan.jpg", "respublika.jpg", "speakeasy.jpg", "morris.jpg", "istorii.jpg", "klassik.jpg", "love.jpg", "shok.jpg", "bochka.jpg"]
+    let secur:String = "http://zslavman.esy.es/imgdb/sri_"
+    var somePics:Array<String> = []
     var spiner:UIActivityIndicatorView!
     
     
-
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // избавляемся от пустых ячеек
         tableView.tableFooterView = UIView(frame: CGRect.zero) // говорим, что футер должен быть 0-вого размера
+        
+        
+        fetchImages()
         
         // перевести операции в основной поток (из фонового, который по умолчанию)
         //        DispatchQueue.main.async {
@@ -76,7 +80,26 @@ class PopularTVC: UITableViewController {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "popularCell", for: indexPath)
         cell.textLabel?.text = someNames[indexPath.row]
-        cell.imageView?.image = UIImage(named: somePics[indexPath.row])
+        // по умолчанию загружаем картинку из проекта
+        cell.imageView?.image = UIImage(named: "photo")
+        //        addSpiner(toItem: cell.imageView!)
+        
+        var link:String = secur
+        
+        if indexPath.row < 10 {
+            link += "0" + String(indexPath.row) + ".jpg"
+        }
+        else{
+            link += String(indexPath.row) + ".jpg"
+        }
+        if let imgURL = URL(string: link){
+            DispatchQueue.main.async {
+                let imgData = NSData(contentsOf: imgURL)
+                cell.imageView?.image = UIImage(data: imgData as! Data)
+                //                self.spiner.stopAnimating()
+            }
+        }
+        
         
         // закруглим изображения
         cell.imageView?.layer.cornerRadius = 10 // аля маска
@@ -86,10 +109,47 @@ class PopularTVC: UITableViewController {
             cell.contentView.backgroundColor = #colorLiteral(red: 0.721867955, green: 0.7081359182, blue: 0.9016706576, alpha: 1)
         }
         
-        addSpiner(toItem: cell.imageView!)
-        
         return cell
     }
+    
+    
+    
+    
+    
+    
+    var DB:[Data] = []
+    
+    func fetchImages() -> Void{
+        
+        for i in 0..<15{
+            var link:String = secur
+            
+            if i < 10 {
+                link += "0" + String(i) + ".jpg"
+            }
+            else{
+                link += String(i) + ".jpg"
+            }
+            if URL(string: link) != nil{
+                DispatchQueue.main.async{
+                    let imgData = try? Data(contentsOf: <#T##URL#>)
+                    self.DB.append(imgData!)
+                }
+                tableView.reloadData()
+            }
+        }
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
@@ -101,49 +161,28 @@ class PopularTVC: UITableViewController {
     
     
     
-    /*
-     // Override to support conditional editing of the table view.
-     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-     // Return false if you do not want the specified item to be editable.
-     return true
-     }
-     */
     
-    /*
-     // Override to support editing the table view.
-     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-     if editingStyle == .delete {
-     // Delete the row from the data source
-     tableView.deleteRows(at: [indexPath], with: .fade)
-     } else if editingStyle == .insert {
-     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-     }
-     }
-     */
     
-    /*
-     // Override to support rearranging the table view.
-     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-     
-     }
-     */
     
-    /*
-     // Override to support conditional rearranging of the table view.
-     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-     // Return false if you do not want the item to be re-orderable.
-     return true
-     }
-     */
     
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destinationViewController.
-     // Pass the selected object to the new view controller.
-     }
-     */
+    
+    
+    
+    
     
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
